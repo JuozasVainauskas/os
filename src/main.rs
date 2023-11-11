1549381d9a1330406a512598843d44fdb2e73079
Juozas Vainauskas <71255955+JuozasVainauskas@users.noreply.github.com>
2023-11-11 16:11:41 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@
 
 // disable all Rust-level entry points
 #![no_main]
-use core::panic::PanicInfo;
 
-mod vga_buffer;
+// Tests related features
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -13,12 +18,29 @@ pub extern "C" fn _start() -> ! {
     // named `_start` by default
 
     println!("System booted successfully");
+
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-// This function is called on panic.
+// This function is called on panic in non-test mode.
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -146,3 +146,10 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("Test");
+    }
+}
